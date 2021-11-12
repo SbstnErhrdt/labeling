965cde27ec659b72bd3601fcd003bd092cb26ddb
Seb <git@erhardt.net>
2021-11-12 11:01:02 +0100

    button

--- pages/organizations/_id/_projectId/_taskId/index.vue
+++ pages/organizations/_id/_projectId/_taskId/index.vue
@@ -19,9 +19,9 @@
       <div v-if="this.LabelingTask"
            class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 
-        {{this.LabelingTask.stats}}
+        <!-- {{ this.LabelingTask.stats }} -->
 
-        <div class="py-4">
+        <div class="py-4 mb-3">
           <NuxtLink :to="{
                 name: 'organizations-id-projectId-taskId-label',
                 params: {
@@ -30,20 +30,27 @@
                   taskId: routeParamTaskId,
                 }
               }"
-                    class="bg-white shadow overflow-hidden rounded-md hover:text-black text-gray-600 font-medium px-6 py-4 hover:bg-gray-200">
-            Start
+                    class="bg-indigo-700 shadow overflow-hidden rounded-md text-white font-medium px-6 py-4 hover:bg-indigo-900">
+            Start Labeling
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
+            </svg>
           </NuxtLink>
         </div>
 
+        <hr>
 
 
-        <div>
+        <div class="pt-5">
+          <h4>Classes</h4>
           <!-- This example requires Tailwind CSS v2.0+ -->
           <ul role="list" class="space-y-3">
             <li v-for="item in this.LabelingTask.classes" :key="item.UID"
                 class="">
-                <span class="text-sm font-medium text-gray-900">{{ item.name }}</span>
-                <p class="text-sm text-gray-500">{{ item.description }}</p>
+              <span class="text-sm font-medium text-gray-900">{{ item.name }}</span><br>
+              <span class="text-sm font-medium " :style="{ 'color': item.color}">{{ item.shortName }}</span>
+              <p class="text-sm text-gray-500">{{ item.description }}</p>
             </li>
             <!-- More items... -->
           </ul>
