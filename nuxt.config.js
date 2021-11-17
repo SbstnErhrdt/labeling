5c3ab3939de1d66ef878f3ba2e03fef71e0acbb6
Seb <git@erhardt.net>
2021-11-17 11:00:23 +0100

    add ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,11 @@
 export default {
+  ssr: false,
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'label',
+    title: 'Label',
     htmlAttrs: {
       lang: 'en'
     },
