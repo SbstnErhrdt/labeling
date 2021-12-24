1a7483a64c3d8cb4bb47c07c3d9e4b9fbed4eb0b
Seb <git@erhardt.net>
2021-12-24 18:03:21 +0100

    start login

---
+++ pages/identity/logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navigation/>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  async mounted() {
+    await this.$apolloHelpers.onLogout();
+    await this.$store.commit('identity/removeCurrentUser');
+    await this.$router.push('/');
+  },
+  methods: {}
+}
+</script>
